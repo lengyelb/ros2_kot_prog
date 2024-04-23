7df779b34e3d346b106e55d5e32e58edba6ff6cf
Balázs Lengyel <lengyel.balazs.t@gmail.com>
2024-04-23 18:14:02 +0200

    modified launch file

--- launch/kot_prog_launch.py
+++ launch/kot_prog_launch.py
@@ -2,15 +2,19 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
+    factory_node = Node(
             package='ros2_kot_prog',
             executable='marker_factory',
             name='marker_factory_node',
-        ),
-        Node(
+        )
+
+    psm_grasp_node = Node(
             package='ros2_kot_prog',
             executable='psm_grasp',
             name='psm_grasp_node',
         )
+
+    return LaunchDescription([
+        factory_node,
+        psm_grasp_node
     ])
